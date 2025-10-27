f9b4d3f44906843424703dc1858e31075c473901
Nathan Albritton <nalbritton@nic.edu>
2025-10-27 10:44:22 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
+void say(string s);
+
 int main()
 {
 
     cout << "Greetings" << endl;
     cout << "Hello world!" << endl;
     return 0;
+}
+
+void say(string s)
+{
+
+    cout << s << endl;
 }
